09b624142ade1cacd7e5aa660c9e4ce306ac2a4c
Klaf <dragnic@gmx.de>
2015-04-17 17:23:21 +0200

    Creating DragnicLeagueBundle

---
+++ src/Dragnic/LeagueBundle/DragnicLeagueBundle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dragnic\LeagueBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DragnicLeagueBundle extends Bundle
+{
+    const NAME = 'DragnicLeagueBundle';
+}
